1882c3aa90bac62a3f7212b5686162a604f84fbb
Nolen Robinson <149610446+nrobinson4@users.noreply.github.com>
2024-03-19 11:59:37 -0500

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,305 @@
+const gameData = [
+    {
+        "Author": "devan4264",
+        "Game Name": "improved-octo-chainsaw-charlie",
+        "URL to game repo": "https://github.com/devan4264/improved-octo-chainsaw-charlie"
+    },
+    {
+        "Author": "devan4264",
+        "Game Name": "verbose-guacamole-delta",
+        "URL to game repo": "https://github.com/devan4264/verbose-guacamole-delta"
+    },
+    {
+        "Author": "Mackenzie Thompson",
+        "Game Name": "legendary-goggles-charlie",
+        "URL to game repo": "https://github.com/mackenzie24t/legendary-goggles-charlie"
+    },
+    {
+        "Author": "Mackenzie Thompson",
+        "Game Name": "delta-jubilant-doodle",
+        "URL to game repo": "https://github.com/mackenzie24t/delta-jubilant-doodle"
+    },
+    {
+        "Author": "JohnAllenB",
+        "Game Name": "crispy-charlie",
+        "URL to game repo": "crispy-charlie-lab03-blaxton"
+    },
+    {
+        "Author": "JohnAllenB",
+        "Game Name": "decatur-delta",
+        "URL to game repo": "decatur-delta-lab04-blaxton"
+    },
+    {
+        "Author": "Kalani Sorensen",
+        "Game Name": "charlie-lab-cis376",
+        "URL to game repo": "https://github.com/babayaga1013/charlie-lab-cis376"
+    },
+    {
+        "Author": "",
+        "Game Name": "reimagined-couscous–delta",
+        "URL to game repo": "https://github.com/babayaga1013/reimagined-couscous--delta"
+    },
+    {
+        "Author": "Jacob-Vance",
+        "Game Name": "turbo-pancake-charlie",
+        "URL to game repo": "https://jacob-vance.github.io/turbo-pancake-charlie/"
+    },
+    {
+        "Author": "Jacob-Vance",
+        "Game Name": "fluffy-pancake-delta",
+        "URL to game repo": "https://jacob-vance.github.io/fluffy-pancake-delta/"
+    },
+    {
+        "Author": "Jayden Green",
+        "Game Name": "delta-jubilant-octo-winner",
+        "URL to game repo": "https://jaydengreen23.github.io/delta-jubilant-octo-winner/"
+    },
+    {
+        "Author": "Jayden Green",
+        "Game Name": "fictional-broccoli-charlie",
+        "URL to game repo": "https://jaydengreen23.github.io/fictional-broccoli-charlie/"
+    },
+    {
+        "Author": "Sam Maldonado",
+        "Game Name": "Hot or Cold",
+        "URL to game repo": "https://smaldonado1326.github.io/cuddly-invention--charlie/"
+    },
+    {
+        "Author": "Sam Maldonado",
+        "Game Name": "Name Game",
+        "URL to game repo": "https://smaldonado1326.github.io/glowing-adventure-delta/"
+    },
+    {
+        "Author": "Nolen Robinson",
+        "Game Name": "Charlie Meme",
+        "URL to game repo": "https://nrobinson4.github.io/bug-free-potato-charlie/"
+    },
+    {
+        "Author": "Nolen Robinson",
+        "Game Name": "Delta Names",
+        "URL to game repo": "https://nrobinson4.github.io/cool-animal-delta/"
+    },
+    {
+        "Author": "Michael Hayden Duran",
+        "Game Name": "The Mystical Lucky Eccentric Silly Llama Game!",
+        "URL to game repo": "https://github.com/MichaelHaydenDuran/chica-bonita-delta"
+    },
+    {
+        "Author": "Michael Hayden Duran",
+        "Game Name": "Charlie Lab Doge Game",
+        "URL to game repo": "https://github.com/MichaelHaydenDuran/silly-dino-charlie"
+    },
+    {
+        "Author": "Alex Glenn",
+        "Game Name": "charlie-project-alex",
+        "URL to game repo": "https://github.com/pglenn1/charlie-project-alex"
+    },
+    {
+        "Author": "Alex Glenn",
+        "Game Name": "delta-project-alex",
+        "URL to game repo": "https://pglenn1.github.io/delta-project-alex/"
+    },
+    {
+        "Author": "Kevin Hernandez",
+        "Game Name": "Charlie-BravoSoMuchWow",
+        "URL to game repo": "https://hkevin555.github.io/lab-charlie/"
+    },
+    {
+        "Author": "Kevin Hernandez",
+        "Game Name": "Spirit Animal Name Game",
+        "URL to game repo": "https://hkevin555.github.io/DeltaLab/"
+    },
+    {
+        "Author": "Marvin De La Cruz",
+        "Game Name": "Delta Lab Spirit animal",
+        "URL to game repo": "https://marvin-2056.github.io/Delta-CIS-367/"
+    },
+    {
+        "Author": "Rodnesia Goodloe",
+        "Game Name": "nesia-charlie-lab",
+        "URL to game repo": "https://github.com/nesiagoodloe/nesia-charlie-lab"
+    },
+    {
+        "Author": "Rodnesia Goodloe",
+        "Game Name": "currentDeltaLab",
+        "URL to game repo": "https://github.com/nesiagoodloe/currentDeltaLab"
+    },
+    {
+        "Author": "Claire Mitchell",
+        "Game Name": "Charlie",
+        "URL to game repo": "https://cm1803.github.io/charlie/"
+    },
+    {
+        "Author": "Claire Mitchell",
+        "Game Name": "Delta",
+        "URL to game repo": "https://cm1803.github.io/delta/"
+    },
+    {
+        "Author": "Saleh Basalim",
+        "Game Name": "Delta-lab",
+        "URL to game repo": "https://salehmb89.github.io/delta-lab/"
+    },
+    {
+        "Author": "Saleh Basalim",
+        "Game Name": "Charlie-lab",
+        "URL to game repo": "https://salehmb89.github.io/waffle-charlie/"
+    },
+    {
+        "Author": "Mandy McGinnis",
+        "Game Name": "Hopping Zone Charlie",
+        "URL to game repo": "https://github.com/mmandelyn/silver-seal-charlie"
+    },
+    {
+        "Author": "Mandy McGinnis",
+        "Game Name": "Spirit Animal Delta",
+        "URL to game repo": "https://github.com/mmandelyn/pink-panda-delta"
+    },
+    {
+        "Author": "Skyler Saunders",
+        "Game Name": "super-adventure-charlie",
+        "URL to game repo": "https://skysaunders.github.io/super-adventure-charlie/"
+    },
+    {
+        "Author": "Skyler Saunders",
+        "Game Name": "animated-sniffle-foxtrot",
+        "URL to game repo": "https://skysaunders.github.io/animated-sniffle-foxtrot/"
+    },
+    {
+        "Author": "Candice Buford",
+        "Game Name": "Glowing Meme Foxtrot",
+        "URL to game repo": "https://cbuf0rd.github.io/glowing-meme-foxtrot/"
+    },
+    {
+        "Author": "Candice Buford",
+        "Game Name": "Glowing Giggle Echo Lab",
+        "URL to game repo": "https://cbuf0rd.github.io/glowing-giggle-echo-lab/"
+    },
+    {
+        "Author": "Mohammed Alrasheidi",
+        "Game Name": "Delta",
+        "URL to game repo": "https://mohammedalrasheidi.github.io/Delta-lab/"
+    },
+    {
+        "Author": "Mohammed Alrasheidi",
+        "Game Name": "Charlie",
+        "URL to game repo": "https://mohammedalrasheidi.github.io/Charlie//"
+    },
+    {
+        "Author": "Grant Hartley",
+        "Game Name": "charlie",
+        "URL to game repo": "https://github.com/hrantgartley/didactic-waffle-delta"
+    },
+    {
+        "Author": "Grant Hartley",
+        "Game Name": "delta",
+        "URL to game repo": "https://github.com/hrantgartley/didactic-waffle-delta"
+    },
+    {
+        "Author": "Grant Hartley",
+        "Game Name": "echo",
+        "URL to game repo": "https://github.com/hrantgartley/jubilant-bassoon-echo"
+    },
+    {
+        "Author": "Grant Hartley",
+        "Game Name": "foxtrot",
+        "URL to game repo": "https://github.com/hrantgartley/lesser-acorn-foxtrot"
+    },
+    {
+        "Author": "Ayman Alluhaybi",
+        "Game Name": "delta",
+        "URL to game repo": "https://aalluhaybi1.github.io/DELTA-LAB/"
+    },
+    {
+        "Author": "Ayman Alluhaybi",
+        "Game Name": "CHARLIE",
+        "URL to game repo": "https://aalluhaybi1.github.io/CHARLIE_LAB/"
+    },
+    {
+        "Author": "Brayden Bolton",
+        "Game Name": "Charlie",
+        "URL to game repo": "https://github.com/bbolton1/CIS376-Charlie-Brayden"
+    },
+    {
+        "Author": "Brayden Bolton",
+        "Game Name": "Delta",
+        "URL to game repo": "https://github.com/bbolton1/delta-lab-brayden"
+    },
+    {
+        "Author": "Navraj_Singh",
+        "Game Name": "probable-charlie",
+        "URL to game repo": "https://github.com/QuantumCoder-242/probable-charlie"
+    },
+    {
+        "Author": "Navraj_Singh",
+        "Game Name": "turbo-delta",
+        "URL to game repo": "https://github.com/QuantumCoder-242/turbo-delta"
+    },
+    {
+        "Author": "AkashdeepSingh0047",
+        "Game Name": "psychic-delta",
+        "URL to game repo": "https://github.com/AkashdeepSingh0047/psychic-delta?tab=readme-ov-file"
+    },
+    {
+        "Author": "AkashdeepSingh0047",
+        "Game Name": "Chrlie-lab-By-Akashdeep",
+        "URL to game repo": "https://github.com/AkashdeepSingh0047/Chrlie-lab-By-Akashdeep?tab=readme-ov-file"
+    },
+    {
+        "Author": "zrundle (Zach Rundle)",
+        "Game Name": "charlie-lab-zrundle",
+        "URL to game repo": "https://github.com/zrundle/charlie-lab-zrundle"
+    },
+    {
+        "Author": "zrundle (Zach Rundle)",
+        "Game Name": "zrundle-delta-lab",
+        "URL to game repo": "https://github.com/zrundle/zrundle-delta-lab"
+    }
+]
+
+
+// Function to create Bootstrap card elements
+function createCard(author, gameName, url) {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('col-md-4', 'mt-3');
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.textContent = gameName;
+
+    const subtitle = document.createElement('h6');
+    subtitle.classList.add('card-subtitle', 'mb-2', 'text-muted');
+    subtitle.textContent = author;
+
+    const link = document.createElement('a');
+    link.classList.add('card-link');
+    link.href = url;
+    link.textContent = 'URL: ' + url;
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(subtitle);
+    cardBody.appendChild(link);
+
+    card.appendChild(cardBody);
+    cardDiv.appendChild(card);
+
+    return cardDiv;
+}
+
+// Function to render cards
+function renderCards(data) {
+    const container = document.querySelector('.container .row');
+    data.forEach(item => {
+        const card = createCard(item['Author'], item['Game Name'], item['URL to game repo']);
+        container.appendChild(card);
+    });
+}
+
+// Call the render function
+renderCards(data);
+
